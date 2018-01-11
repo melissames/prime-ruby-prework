bb61d1c00331519a6d93f2b89938185907e8ef64
Melissa Sames <melissa.sames@gmail.com>
2018-01-11 20:55:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  else (2..Math.sqrt(num)).each { |i|
+    if num % i == 0 && i < num
+      return false
+    end
+    }
+  end
+  true
+end
